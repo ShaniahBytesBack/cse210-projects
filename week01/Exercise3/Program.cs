3b579a2b735c58724fdbe38ccb507bcd0d25aaf9
ShaniahBytesBack <snyabawa@byui.edu>
2025-01-20 13:12:52 -0800

    Implement Guess My Number game with replay functionality

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -2,8 +2,77 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("Welcome to the Guess My Number game!");
+
+        PlayGame();
+    }
+
+    static void PlayGame()
+    {
+        Random random = new Random();
+
+        int magicNumber = random.Next(1, 101);
+
+        int guess = 0;
+
+        int numberOfGuesses = 0;
+
+        int maxGuesses = 10;
+
+        while (guess != magicNumber && numberOfGuesses < maxGuesses)
+        {
+            Console.WriteLine($"\nAttempt {numberOfGuesses + 1} of {maxGuesses}.");
+            Console.Write("What is your guess? ");
+
+            bool validInput = int.TryParse(Console.ReadLine(), out guess);
+
+            if (!validInput)
+            {
+                Console.WriteLine("Invalid input! Please enter a valid number.");
+                continue; // Skip the rest of the loop and prompt the user again
+            }
+
+            numberOfGuesses++;
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine($"You guessed it! The magic number was {magicNumber}.");
+                Console.WriteLine($"It took you {numberOfGuesses} guesses.");
+                break;
+            }
+
+            if (numberOfGuesses == maxGuesses)
+            {
+                Console.WriteLine($"Sorry, you've reached the maximum number of guesses. The magic number was {magicNumber}.");
+                break;
+            }
+        }
+
+        PlayAgain();
+    }
+
+    static void PlayAgain()
+    {
+        Console.Write("\nDo you want to play again? (yes/no): ");
+        string response = Console.ReadLine().ToLower();
+
+        if (response == "yes")
+        {
+            PlayGame();
+        }
+        else
+        {
+            Console.WriteLine("Thanks for playing! Goodbye.");
+        }
     }
 }
