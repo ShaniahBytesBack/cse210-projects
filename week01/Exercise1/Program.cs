bdcd0c061ed85f43f71eb21c55de030dd1e6cb0f
ShaniahBytesBack <snyabawa@byui.edu>
2025-01-20 12:48:10 -0800

    Update Program.cs to prompt for user names and display full name

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Prompts user for first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Prompts the user for last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Prints their full name
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
